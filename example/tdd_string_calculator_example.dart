f102a1880f8d9a6ef55554abbc72b83845c35b9f
Rohit Kokate <kokate.rohit17@gmail.com>
2025-08-06 15:27:12 +0530

    Update example.

--- example/tdd_string_calculator_example.dart
+++ example/tdd_string_calculator_example.dart
@@ -3,4 +3,7 @@ import 'package:tdd_string_calculator/tdd_string_calculator.dart';
 void main() {
   final StringCalculator stringCalculator = StringCalculatorImpl();
   print(stringCalculator.add(''));
+  print(stringCalculator.add('//|\n23|42|2|12|'));
+  print(stringCalculator.add('\n23,32,12'));
+  print(stringCalculator.add('//|\n23|-42|2|-12|'));
 }
