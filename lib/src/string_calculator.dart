1f5c9f8cc0b7ae9d0ec5d107589aa70716f0c026
Rohit Kokate <kokate.rohit17@gmail.com>
2025-08-06 13:25:35 +0530

    String Calculator: should return 0 on passing empty string

--- lib/src/string_calculator.dart
+++ lib/src/string_calculator.dart
@@ -4,6 +4,9 @@ abstract class StringCalculator {
 
 class StringCalculatorImpl implements StringCalculator {
   int add(String text) {
+    if (text.isEmpty) {
+      return 0;
+    }
     return -1;
   }
 }
--- test/tdd_string_calculator_test.dart
+++ test/tdd_string_calculator_test.dart
@@ -2,14 +2,14 @@ import 'package:tdd_string_calculator/tdd_string_calculator.dart';
 import 'package:test/test.dart';
 
 void main() {
-  group('A group of tests', () {
+  group('String Calculator', () {
     final StringCalculator stringCalculator = StringCalculatorImpl();
 
     setUp(() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
+    test('should return 0 on passing empty string', () {
       expect(stringCalculator.add(''), 0);
     });
   });
